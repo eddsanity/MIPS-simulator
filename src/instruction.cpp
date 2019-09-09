0e4027e545a8eb205c922accef35a87a83aa24c9
eddsanity <eyadhossamm@gmail.com>
2019-09-09 10:12:03 +0200

    made some progress in the deduceInstr function, finished R-Type instruction deduction

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -4,7 +4,8 @@ Instruction::Instruction(uint32_t _mcode)
 {
 	instr = _mcode;           
 	opcode = subword(_mcode, 31, 26);
-	deduceFormat();           
+	deduceFormat();
+	deduceInstr();
 	switch (instrFormat)
 	{
 	case RTYPE:
@@ -47,8 +48,87 @@ void Instruction::deduceFormat()
 {
 	if (opcode == 0)
 		instrFormat = RTYPE;
-	else if (opcode == 2 || opcode == 3 || opcode == 26)
+	else if (opcode == 2 || opcode == 3)
 		instrFormat = JTYPE;
 	else
 		instrFormat = ITYPE;
+}
+
+void Instruction::deduceInstr()
+{
+	switch (instrFormat)
+	{
+	case RTYPE:
+		switch (funct)
+		{
+		case 0x20:
+			instrToken = ADD;
+			break;
+		case 0x21:
+			instrToken = ADDU;
+			break;
+		case 0x22:
+			instrToken = SUB;
+			break;
+		case 0x23:
+			instrToken = SUBU;
+			break;
+		case 0x1A:
+			instrToken = DIV;
+			break;
+		case 0x1B:
+			instrToken = DIVU;
+			break;
+		case 0x18:
+			instrToken = MULT;
+			break;
+		case 0x19:
+			instrToken = MULTU;
+			break;
+		case 0x24:
+			instrToken = AND;
+			break;
+		case 0x25:
+			instrToken = OR;
+			break;
+		case 0x26:
+			instrToken = XOR;
+			break;
+		case 0x27:
+			instrToken = NOR;
+			break;
+		case 0x0:
+			instrToken = SLL;
+			break;
+		case 0x2:
+			instrToken = SRL;
+			break;
+		case 0x2A:
+			instrToken = SLT;
+			break;
+		case 0x2B:
+			instrToken = SLTU;
+			break;
+		case 0x10:
+			instrToken = MFHI;
+			break;
+		case 0x11:
+			instrToken = MTHI;
+			break;
+		case 0x12:
+			instrToken = MFLO;
+			break;
+		case 0x13:
+			instrToken = MTLO;
+			break;
+		case 0xC:
+			instrToken = SYSCALL;
+			break;
+		}
+		break;
+	case ITYPE:
+		break;
+	case JTYPE:
+		break;
+	}
 }
--- src/utility.hpp
+++ src/utility.hpp
@@ -6,9 +6,9 @@
 enum INSTRFORMAT { RTYPE, ITYPE, JTYPE };
 enum INSTRTOKEN  { ADD, ADDI, ADDIU, ADDU, SUB, SUBU, DIV, DIVU, MULT, MULTU,
                    LB, LW, LUI, SB, SW,
-	               AND, ANDI, OR, ORI, XOR, XORI, SLL, SRL,
-	               SLT, SLTI,
-                   MFLO, MFHI, NOOP, SYSCALL };
+                   AND, ANDI, OR, ORI, XOR, XORI, NOR, SLL, SRL,
+                   SLT, SLTU, SLTI,
+                   MFLO, MTLO, MFHI, MTHI, NOOP, SYSCALL };
 
 uint32_t subword(uint32_t, uint8_t, uint8_t);
 uint32_t signExtend8bit(uint8_t);
