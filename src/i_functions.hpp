720aca62568e9c03362683e761461cc168bb3c21
Eyad <eyadhossamm@gmail.com>
2019-09-16 18:55:26 +0200

    function declaration for I-Type instructions and some OCD alignment magic in the R-Type declaration file

--- src/i_functions.hpp
+++ src/i_functions.hpp
@@ -6,7 +6,27 @@
 #include "memory.hpp"
 #include "utility.hpp"
 
-
-
+uint8_t ADDI (Instruction, Memory&, regFile&);      // 0x08
+uint8_t ADDIU(Instruction, Memory&, regFile&);      // 0x09
+uint8_t ANDI (Instruction, Memory&, regFile&);      // 0x0C
+uint8_t ORI  (Instruction, Memory&, regFile&);      // 0x0D
+uint8_t XORI (Instruction, Memory&, regFile&);      // 0x0E
+uint8_t BEQ  (Instruction, Memory&, regFile&);      // 0x04
+uint8_t BGEZ (Instruction, Memory&, regFile&);      // 0x01, rt = $1
+uint8_t BGTZ (Instruction, Memory&, regFile&);      // 0x07, rt = $0
+uint8_t BLEZ (Instruction, Memory&, regFile&);      // 0x06, rt = $0
+uint8_t BLTZ (Instruction, Memory&, regFile&);      // 0x01, rt = $0
+uint8_t BNE  (Instruction, Memory&, regFile&);      // 0x05
+uint8_t SLTI (Instruction, Memory&, regFile&);      // 0x0A
+uint8_t SLTIU(Instruction, Memory&, regFile&);      // 0x0B
+uint8_t LB   (Instruction, Memory&, regFile&);      // 0x20
+uint8_t LBU  (Instruction, Memory&, regFile&);      // 0x24
+uint8_t LH   (Instruction, Memory&, regFile&);      // 0x21
+uint8_t LHU  (Instruction, Memory&, regFile&);      // 0x25
+uint8_t LUI  (Instruction, Memory&, regFile&);      // 0x0F
+uint8_t LW   (Instruction, Memory&, regFile&);      // 0x23
+uint8_t SB   (Instruction, Memory&, regFile&);      // 0x28
+uint8_t SH   (Instruction, Memory&, regFile&);      // 0x29
+uint8_t SW   (Instruction, Memory&, regFile&);      // 0x2B
 
 #endif
--- src/r_functions.hpp
+++ src/r_functions.hpp
@@ -6,24 +6,24 @@
 #include "memory.hpp"
 #include "utility.hpp"
 
-uint32_t ADD(Instruction, Memory&, regFile&);      // 0x20
-uint32_t ADDU(Instruction, Memory&, regFile&);     // 0x21
-uint32_t SUB(Instruction, Memory&, regFile&);      // 0x22
-uint32_t SUBU(Instruction, Memory&, regFile&);     // 0x23
-uint32_t DIV(Instruction, Memory&, regFile&);      // 0x1A
-uint32_t DIVU(Instruction, Memory&, regFile&);     // 0x1B
-uint32_t MULT(Instruction, Memory&, regFile&);     // 0x18
-uint32_t MULTU(Instruction, Memory&, regFile&);    // 0x19
-uint32_t AND(Instruction, Memory&, regFile&);      // 0x24
-uint32_t OR(Instruction, Memory&, regFile&);       // 0x25
-uint32_t XOR(Instruction, Memory&, regFile&);      // 0x26
-uint32_t NOR(Instruction, Memory&, regFile&);      // 0x27
-uint32_t SLL(Instruction, Memory&, regFile&);      // 0x00
-uint32_t SRL(Instruction, Memory&, regFile&);      // 0x02
-uint32_t SLT(Instruction, Memory&, regFile&);      // 0x2A
-uint32_t SLTU(Instruction, Memory&, regFile&);     // 0x2B
-uint32_t MFHI(Instruction, Memory&, regFile&);     // 0x10
-uint32_t MFLO(Instruction, Memory&, regFile&);     // 0x12
-uint32_t SYSCALL(Instruction, Memory&, regFile&);  // 0x0C
+uint8_t ADD    (Instruction, Memory&, regFile&);      // 0x20
+uint8_t ADDU   (Instruction, Memory&, regFile&);      // 0x21
+uint8_t SUB    (Instruction, Memory&, regFile&);      // 0x22
+uint8_t SUBU   (Instruction, Memory&, regFile&);      // 0x23
+uint8_t DIV    (Instruction, Memory&, regFile&);      // 0x1A
+uint8_t DIVU   (Instruction, Memory&, regFile&);      // 0x1B
+uint8_t MULT   (Instruction, Memory&, regFile&);      // 0x18
+uint8_t MULTU  (Instruction, Memory&, regFile&);      // 0x19
+uint8_t AND    (Instruction, Memory&, regFile&);      // 0x24
+uint8_t OR     (Instruction, Memory&, regFile&);      // 0x25
+uint8_t XOR    (Instruction, Memory&, regFile&);      // 0x26
+uint8_t NOR    (Instruction, Memory&, regFile&);      // 0x27
+uint8_t SLL    (Instruction, Memory&, regFile&);      // 0x00
+uint8_t SRL    (Instruction, Memory&, regFile&);      // 0x02
+uint8_t SLT    (Instruction, Memory&, regFile&);      // 0x2A
+uint8_t SLTU   (Instruction, Memory&, regFile&);      // 0x2B
+uint8_t MFHI   (Instruction, Memory&, regFile&);      // 0x10
+uint8_t MFLO   (Instruction, Memory&, regFile&);      // 0x12
+uint8_t SYSCALL(Instruction, Memory&, regFile&);   // 0x0C
 
 #endif
